652c9b89c966fb344350caa893d236051935d60e
miriamcsn <mceli87@gmail.com>
2021-08-03 13:31:37 -0300

    Implement Order class

--- src/main/java/com/myfactory/webservicesjpa/entities/User.java
+++ src/main/java/com/myfactory/webservicesjpa/entities/User.java
@@ -1,12 +1,12 @@
 package com.myfactory.webservicesjpa.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     @Id
@@ -16,6 +16,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -67,6 +70,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
