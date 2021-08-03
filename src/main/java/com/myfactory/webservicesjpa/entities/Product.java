896e0ef07134a44a323bdff660baa0c6ee33bd27
miriamcsn <mceli87@gmail.com>
2021-08-03 15:31:55 -0300

    Create JoinTable between Category and Product - ManyToMany

--- src/main/java/com/myfactory/webservicesjpa/entities/Product.java
+++ src/main/java/com/myfactory/webservicesjpa/entities/Product.java
@@ -16,7 +16,10 @@ public class Product implements Serializable {
     private String name, description, imgURL;
     private Double price;
 
-    @Transient
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
     // por que set e não uma lista?
     // porque lista aceita repetições, set não - e precisamos garantir que a categoria não se repita.
